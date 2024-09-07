1cbb6ec6794839fabfa4fe1afc2bb2aba32a7965
Sushanta Sahu <sushantasahu175@gmail.com>
2024-09-07 10:20:55 +0530

    create main.go file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,16 @@
 package main
 
+import(
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/Sushanta175/Book-Management-System/pkg/routes"
+)
+
 func main () {
-	
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":9010", r))
 }
